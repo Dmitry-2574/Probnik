7856e2983ba3aa37dcb962ea436a59397a8e9501
Dim <d@mal>
2024-12-01 12:48:55 +0700

    еще одну функцию написал попробовал

--- func_1.py
+++ func_1.py
@@ -15,4 +15,24 @@ def fuc(name, city, age=35):
     return f'Твое имя {name}\nГород: {city}'
 
 print(fuc('дмитрий', 'барнаул', 45))
-# print(fuc(35, 56, "дмитрий"))   
+# print(fuc(35, 56, "дмитрий")) 
+# 
+# 
+# 
+def main(nuk, name, hop):
+  hop_1 = ''
+  if not isinstance(nuk, int):
+    raise TypeError('Нужно ввести число')
+  if not isinstance(name, str):
+    raise TypeError('Должно быть строкой')
+  if not isinstance(hop, str):
+    raise TypeError('Должно быть строкой')
+  
+  if nuk>100:
+    hop_1 += (f'{name} из города {hop} У тебя доостаточно средств: {nuk}')
+  if nuk<100:
+    hop_1 += (f'{name} из города {hop} у тебе не хватает, внеси еще')
+  return hop_1
+f = main(700, 'дмитрий', 'барнаул')
+print(f)
+  
